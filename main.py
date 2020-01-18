770cd83164888632b62d0822396377beddca5e23
chinablue206 <chinablue206@gmail.com>
2020-01-18 23:00:18 +0900

    remote unnecessary print

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from github import Github
 import re
 import sys
 
-args = sys.argv
-print(args)
+# parse sp label
 def get_sp(labels):
     for l in labels:
         if(re.match('\d+', l.name)):
@@ -11,7 +10,7 @@ def get_sp(labels):
 
 g = Github()
 
-for i in g.search_issues(f"repo:rsks/circlecitest is:open label:{args[1]}"):
+for i in g.search_issues(f"repo:rsks/circlecitest is:open label:{sys.argv[1]}"):
     title=i.title
     assignee=i.assignees[0].login
     sp=get_sp(i.labels)
